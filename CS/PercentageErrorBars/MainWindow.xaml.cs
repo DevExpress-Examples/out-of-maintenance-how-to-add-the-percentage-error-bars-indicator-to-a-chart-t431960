dd7455323de93be6320a4891f4275e9f199ba934
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/PercentageErrorBars/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace ErrorBars {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
+//        List<DataItem> GetData() {
+//            return new List<DataItem>() {
+//                new DataItem("A", 20),
+//                new DataItem("B", 50),
+//                new DataItem("C", 40),
+//                new DataItem("D", 22),
+//                new DataItem("E", 30),
+//                new DataItem("F", 45),
+//                new DataItem("G", 35),
+//                new DataItem("H", 28),
+//                new DataItem("I", 46),
+//                new DataItem("J", 27),
+//                new DataItem("K", 20),
+//                new DataItem("L", 50),
+//                new DataItem("M", 40),
+//                new DataItem("N", 22),
+//                new DataItem("O", 30),
+//                new DataItem("P", 45),
+//                new DataItem("Q", 35),
+//            };
+//        }
+//        private void OnLoaded(object sender, RoutedEventArgs e) {
+//            if (pointSeries != null) {
+//                pointSeries.ArgumentDataMember = "Argument";
+//                pointSeries.ValueDataMember = "Value";
+//                pointSeries.DataSource = GetData();
+//            }
+//        }
+//    }
+//    public class DataItem {
+//        public string Argument { get; private set; }
+//        public int Value { get; private set; }
+//        public DataItem(string argument, int value) {
+//            Argument = argument;
+//            Value = value;
+//        }
+//    }
+//}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to: Add the Percentage Error Bars indicator to a chart
+
+
+<p>This example demonstrates how to plot Percentage Error Bars on the chart.</p>
+
+
+<h3>Description</h3>
+
+To do this, add a&nbsp;<a href="https://documentation.devexpress.com/#WPF/clsDevExpressXpfChartsPercentageErrorBarstopic">PercentageErrorBars</a>&nbsp;object to the series&nbsp;<a href="https://documentation.devexpress.com/#WPF/DevExpressXpfChartsXYSeries2D_Indicatorstopic">XYSeries2D.Indicators</a>&nbsp;collection.&nbsp;Then, define&nbsp;the percentage of error values using the&nbsp;<a href="https://documentation.devexpress.com/#WPF/DevExpressXpfChartsPercentageErrorBars_Percenttopic">PercentageErrorBars.Percent</a>&nbsp;property.
+
+<br/>
+
+
